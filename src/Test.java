4f35a1e271e77c0fd0f185dc63c3a81abd255918
COM <COM@DESKTOP-TH36TT5>
2020-11-17 16:16:37 +0900

    commit

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,7 @@ public class Test {
 	//content1
 	//content2
 	//content3
-<<<<<<< HEAD
+	//content4	
 	//content5
-=======
-	//content4
->>>>>>> branch 'master' of https://github.com/dch0505/TestProject
+
 }
